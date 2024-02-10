ad70646bcc52fff281b67e773aecc68280f4572b
Salah <salah@MacBook-Pro-de-Salah.local>
2024-02-10 17:07:17 +0100

    add multiple filter :catagory, price..

--- mon-app-react/src/apiService.js
+++ mon-app-react/src/apiService.js
@@ -8,10 +8,24 @@ export const getBooks = async () => {
       throw new Error(`Erreur HTTP: ${response.status}`);
     }
     
-    const data = await response.json();  // Utilisation de `await` et déclaration de `data`
+    const data = await response.json();
     return data['hydra:member'] || [];
   } catch (error) {
     console.error("Erreur lors de la récupération des données: ", error);
     return [];
+  }
+};
+
+
+export const getCategories = async () => {
+  try {
+    const response = await fetch('http://localhost:8090/api/categories');
+    if (!response.ok) throw new Error('Network response was not ok');
+    const data = await response.json();
+    return data["hydra:member"];
+    console.log(data["hydra:member"]);
+  } catch (error) {
+    console.error("There was a problem with the fetch operation: ", error.message);
+    throw error;
   }
 };
--- mon-app-react/src/components/Book.js
+++ mon-app-react/src/components/Book.js
@@ -3,54 +3,115 @@ import React, { useEffect, useState } from 'react';
 import useBooks from '../hook/useBooks';
 import Rating from './Rating';
 import '../style/Book.css'; // Assurez-vous que le chemin est correct
+import { getCategories } from '../apiService';
 
 function Book() {
     const [query, setQuery] = useState("");
     const { books, loading, error } = useBooks();
     const [filteredBooks, setFilteredBooks] = useState([]);
 
+    const [currentPrice, setCurrentPrice] = useState(20);
+    const maxPrice = 100;
+    const [selectedCategory, setSelectedCategory] = useState(""); // Add this line
+    const [categories, setCategories] = useState([]);
+    // Mise à jour de useEffect pour inclure la logique de filtrage par prix
     useEffect(() => {
         if (!loading && books) {
-            const filtered = query === '' ? books : books.filter(book =>
-                book.title.toLowerCase().includes(query.toLowerCase())
-            );
+            const filtered = books.filter(book => {
+                const isMatchTitle = book.title.toLowerCase().includes(query.toLowerCase());
+                const isMatchPrice =   book.price >= currentPrice && book.price <= maxPrice;
+                const isMatchCategory = selectedCategory ? book.categoryName === selectedCategory : true;
+                return isMatchTitle && isMatchPrice && isMatchCategory;
+            });
             setFilteredBooks(filtered);
         }
-    }, [query, books, loading]);
+    }, [query, books, loading, currentPrice, maxPrice, selectedCategory]);
 
     const handleSearchChange = (e) => {
         setQuery(e.target.value);
     };
 
-    // Générez un numéro aléatoire pour l'image
+    const handlePriceFilterChange = (e) => {
+        setCurrentPrice(Number(e.target.value));
+    };
+
     const getRandomImage = () => {
-        const number = Math.floor(Math.random() * 4) + 1; // Génère un nombre entre 1 et 4
+        const number = Math.floor(Math.random() * 4) + 1;
         return `/images/book${number}.webp`;
     };
+
+    useEffect(() => {
+        const fetchCategories = async () => {
+            try {
+                const categoryData = await getCategories();
+                setCategories(categoryData);
+            } catch (error) {
+
+            }
+        };
+
+        fetchCategories();
+    }, []);
 
     if (loading) return <div>Chargement...</div>;
     if (error) return <div>Erreur: {error}</div>;
 
     return (
-        <div className="book-list">
-            <input
-                type="text"
-                placeholder="Rechercher des livres..."
-                value={query}
-                onChange={handleSearchChange}
-                className="search-input"
-            />
-            <div className="books-grid">
-                {filteredBooks.map(book => (
-                    <div key={book.id} className="book-item">
-                        <img src={getRandomImage()} alt={book.title} className="book-image" />
-                        <div className="book-details">
-                            <h3 className="book-title">{book.title}</h3>
-                            <p className="book-author">{book.Author.name}</p>
-                            <Rating bookId={book.id} onRating={(rating) => console.log(rating)} />
+        <div className="container">
+            <aside className="sidebar">
+                <input
+                    type="text"
+                    placeholder="Rechercher des livres..."
+                    value={query}
+                    onChange={handleSearchChange}
+                    className="search-input"
+                />
+                {/* Ajout des entrées pour la plage de prix */}
+                <div className="price-slider-container">
+                    <input
+                        type="range"
+                        min="20"
+                        max={maxPrice}
+                        value={currentPrice}
+                        onChange={handlePriceFilterChange}
+                        className="price-slider"
+                    />
+                    <div className="price-slider-value">{currentPrice} €</div>
+                </div>
+                <select
+                    value={selectedCategory}
+                    onChange={(e) => setSelectedCategory(e.target.value)}
+                    className="category-select"
+                >
+                    <option value="">Toutes les catégories</option>
+                    {categories.map((category) => (
+                        <option key={category.id} value={category.id}>
+                            {category.name}
+                        </option>
+                    ))}
+                </select>
+            </aside>
+            <div className="book-list">
+                <input
+                    type="text"
+                    placeholder="Rechercher des livres..."
+                    value={query}
+                    onChange={handleSearchChange}
+                    className="search-input"
+                />
+                <div className="books-grid">
+                    {filteredBooks.map(book => (
+                        <div key={book.id} className="book-item">
+                            <img src={getRandomImage()} alt={book.title} className="book-image" />
+                            <div className="book-details">
+                                <h3 className="book-title">{book.title}</h3>
+                                <p className="book-author">{book.Author.name}</p>
+                                <Rating bookId={book.id} onRating={(rating) => console.log(rating)} />
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
+
             </div>
         </div>
     );
